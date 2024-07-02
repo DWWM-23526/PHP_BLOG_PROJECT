d6769c7653c393ae43dc65002ab0a807463bf830
laurentbedu <bedulaurent@gmail.com>
2024-07-02 10:51:51 +0200

    Step03

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # PHP_BLOG_PROJECT
 
 Branch Step01 : Présentation du projet et mise en place de la DB  
-Branch Step02 : Présentation du projet et mise en place de la DB 
+Branch Step02 : Premières pages en PHP 
+Branch Step03 : Méthodes GET et POST 
--- app/article.php
+++ app/article.php
@@ -45,23 +45,12 @@
         <?php include_once "navbar.php"; ?>
         <main class="mt-5 pt-3 row">
             <div class="col-12">
-                <?= $_GET['id']; ?>
-            </div>
-        </main>
-    </div>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>  
-</body>
-
-</html>
-
-
-<!-- <div class="text-center mb-3">
+                <div class="text-center mb-3">
                     <h1><?= $article['title'] ?></h1>
                     <img src="<?= $article['img_src'] ?>" alt="<?= $article['title'] ?>">
                 </div>
                 <p style="text-align: justify;">
-                    <?= $article['summary']?>
+                <?= $article['summary']?>
                 </p>
                 <p>
                     Publié le <?= date("d/m/Y", strtotime($article['published_at']));?>
@@ -72,7 +61,18 @@
                 <?php        
                     }
                 ?>
-                </p> -->
+                </p>
+            </div>
+        </main>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>  
+</body>
+
+</html>
+
+
+
 
 
 
